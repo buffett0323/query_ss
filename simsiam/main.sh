5b9238aa29c956604544993fb7801700aeda243c
buffett0323 <philip910323@gmail.com>
2025-02-11 08:39:43 +0000

    rm

--- simsiam/main.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 python train_new.py \
-    --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
-    --fix-pred-lr \
-    # [your imagenet-folder with train and val folders]
