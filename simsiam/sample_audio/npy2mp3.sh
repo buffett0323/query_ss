b7e90222af20c346302e89419eb48a642d834a06
buffett <philip910323@gmail.com>
2025-04-28 16:26:17 +0000

    Training finish

--- simsiam/sample_audio/npy2mp3.sh
+++ simsiam/sample_audio/npy2mp3.sh
@@ -1,4 +1,8 @@
-python npy2mp3.py --output_mp3 target.mp3    /mnt/gestalt/home/buffett/beatport_analyze/chorus_audio_16000_095sec_npy/dfcf0af5-3a9c-4c33-8a49-ec66c04e405a_2/other_seg_0.npy
-python npy2mp3.py --output_mp3 top1.mp3    /mnt/gestalt/home/buffett/beatport_analyze/chorus_audio_16000_095sec_npy/8586e1cf-36af-4c9a-a331-c1aeaa8cd275_2/other_seg_0.npy
-python npy2mp3.py --output_mp3 top2.mp3    /mnt/gestalt/home/buffett/beatport_analyze/chorus_audio_16000_095sec_npy/ff94ac66-810e-4b4f-ae3e-160ec8b2ea24_4/other_seg_0.npy
-python npy2mp3.py --output_mp3 top3.mp3    /mnt/gestalt/home/buffett/beatport_analyze/chorus_audio_16000_095sec_npy/b8bbd28e-012f-4286-88cd-d5b0deb1a2b6_4/other_seg_0.npy
+python npy2mp3.py --output_mp3 target.mp3    /mnt/gestalt/home/buffett/beatport_analyze/chorus_audio_16000_095sec_npy/98b0b744-f765-4828-b04b-767d877d7adc_5/other_seg_0.npy
+python npy2mp3.py --output_mp3 top1.mp3    /mnt/gestalt/home/buffett/beatport_analyze/chorus_audio_16000_095sec_npy/2860778c-2fc6-4e24-bca7-c8b48f36d30a_4/other_seg_0.npy
+python npy2mp3.py --output_mp3 top2.mp3    /mnt/gestalt/home/buffett/beatport_analyze/chorus_audio_16000_095sec_npy/c94f5542-2ae9-424f-9f51-9f89c6d2285a_4/other_seg_0.npy
+python npy2mp3.py --output_mp3 top3.mp3    /mnt/gestalt/home/buffett/beatport_analyze/chorus_audio_16000_095sec_npy/8d6ff395-eb32-4358-a73c-0bb91e64d752_1/other_seg_0.npy
+
+python npy2mp3.py --output_mp3 test_target1.mp3    /mnt/gestalt/home/buffett/beatport_analyze/chorus_audio_16000_095sec_npy/9043b8ad-25ed-4351-968e-32919925ef08_1/other_seg_0.npy
+python npy2mp3.py --output_mp3 test_target2.mp3    /mnt/gestalt/home/buffett/beatport_analyze/chorus_audio_16000_095sec_npy/f846b840-5a2c-4d32-9d88-78dd63fc9422_1/other_seg_0.npy
+python npy2mp3.py --output_mp3 test_target3.mp3    /mnt/gestalt/home/buffett/beatport_analyze/chorus_audio_16000_095sec_npy/7edcd457-9f30-4570-8f2c-b30068f0c22c_3/other_seg_0.npy
