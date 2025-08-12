73a2fa7ccb12cfb57339cf11defcc07c3322e51a
buffett0323 <philip910323@gmail.com>
2025-08-12 04:30:56 +0000

    Training Proposed Revision

--- edm_fac/eval_proposed_detail.sh
+++ edm_fac/eval_proposed_detail.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICES=1 python eval_proposed_detail.py \
+CUDA_VISIBLE_DEVICES=2 python eval_proposed_detail.py \
     --device cuda:0 \
     --bs 32 \
     --config configs/config_proposed_no_ca.yaml \
-    --checkpoint /mnt/gestalt/home/buffett/EDM_FAC_LOG/0804_proposed_no_ca/ckpt/checkpoint_latest.pt \
-    --output_dir /mnt/gestalt/home/buffett/EDM_FAC_LOG/final_eval/0804_proposed_no_ca/detail/checkpoint_latest
+    --checkpoint /home/buffett/nas_data/EDM_FAC_LOG/0804_proposed_no_ca/ckpt/checkpoint_400000.pt \
+    --output_dir /home/buffett/nas_data/EDM_FAC_LOG/final_eval/0804_proposed_no_ca/detail/checkpoint_latest
